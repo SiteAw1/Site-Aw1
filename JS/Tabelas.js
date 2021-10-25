b5fb4c5cfc5b0ab7e9f063b672e9a057ff639ba8
AloStawi <isabelle.nascimento@aluno.ifsp.edu.br>
2021-10-25 20:39:08 -0300

    Add files via upload

---
+++ JS/Tabelas.js
@@ -0,0 +1,24 @@
+function showDiv1() 
+        {
+                document.getElementById('table1').style.display = "flex";
+        }
+function showDiv2() 
+        {
+                document.getElementById('table2').style.display = "flex";
+        }
+function showDiv3() 
+        {
+                document.getElementById('table3').style.display = "flex";
+        }
+function showDiv4() 
+        {
+                document.getElementById('table4').style.display = "flex";
+        }
+function showDiv5() 
+        {
+                document.getElementById('table5').style.display = "flex";
+        }
+function showDiv6()
+        {
+                document.getElementById('table6').style.display = "flex";
+        }
